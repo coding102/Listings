6ed0d0b4f8ff32d893af0a7989db13692316ca25
Marck <mercercountynj@gmail.com>
2023-11-07 17:54:40 -0500

    adding property model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_06_221718) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_220347) do
+  create_table "houses", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.string "category", null: false
+    t.string "address", null: false
+    t.string "image", null: false
+    t.float "price", null: false
+    t.integer "bedroom", null: false
+    t.integer "bathroom", null: false
+    t.integer "car", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_houses_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
