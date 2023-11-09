e09a1214630498c53f520bdb87b3b48ed55ca5e9
Marck <mercercountynj@gmail.com>
2023-11-09 01:42:13 -0500

    house controller index own show create update

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -4,18 +4,43 @@ class HouseController < ApplicationController
   before_action :set_house, only: [:show, :update]
 
   def index      #GET /houses || /houses.json
+    category = params[:category]
+    search = params[:search]
+
+    @houses = House.where
+            ("category LIKE '%#{category}%' AND title LIKE '%#{search}%'")
+            .order(created_at: :desc)
+    render json: @houses
   end
 
   def own        #GET properties owned by user /houses/own  ||  /houses/own.json
+    @houses = House.where(user_id: authenticate_user!).order(created_at: :desc)
+    render json: @houses
   end
 
   def show       #GET a specific /houses/1 || /houses/1.json
+    if @house
+      render json: @house
+    else
+      render json: nil, status: :unauthorized
+    end
   end
 
   def create     #POST /houses || /houses.json
+    @house = House.new(house_params)     #NEW house object
+    @house.update_attribute(:user_id, authenticate_user!.id)     #additional attribute
+    @house.save
+
+    render json: @house
   end
 
   def update     #PATCH /houses/1 || /houses/1.json
+    if @house
+      @house.update(house_params)
+      render json: @house
+    else
+      render json: nil, status: :unauthorized
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/houses/own', to: 'houses#own'
+  resources :houses
+
   devise_for :users, controllers: {
     sessions: 'sessions',
     registrations: 'registrations',
